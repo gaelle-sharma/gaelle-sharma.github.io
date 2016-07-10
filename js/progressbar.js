f7cf356fa7ca2f2e2372772fb9242d9aaa6a42bf
Gaelle <gaelle2030@gmail.com>
2016-07-10 10:40:45 -0400

    progress bar updates

---
+++ js/progressbar.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	$.getJSON("./data.json", function( result ) {
+	  var total_count = result.data.length;
+	  var completed_count = 0;
+	  for (i = 0; i < total_count; i++) {
+	  	var status = result.data[i].status;
+	  	if (status=="Complete"){
+	  		completed_count++;
+	  	}
+	  }
+      var progresspercent = (completed_count / total_count) * 100;
+	  $("#progress-bar").width(progresspercent+"%")
+	})
+})
